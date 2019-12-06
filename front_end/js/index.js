08f25077f2c9b5e2416b9f6e50c2a7d90ddbf2bd
Giannis Konstantakis <gkonstan93@gmail.com>
2019-12-06 17:23:38 +0200

    [Front-end] [index] added post ajax request on form submit.

--- front_end/js/index.js
+++ front_end/js/index.js
@@ -1,15 +1,34 @@
+var values = {};
 $(document).ready(function(){
 
     $( "#createAccount" ).submit(function( event ) {
 
-        var values = {};
         $.each($('#createAccount').serializeArray(), function(i, field) {
             values[field.name] = field.value;
         });
         console.log(values);
+
+        var url = serviceURL + "account?customerId=" + values.customerID + "&initialCredit=" + values.initialCredit 
+        $.ajax({
+            url: url,
+            type: 'POST',
+            success: postSuccess,
+            error : postError
+        });
 
         //send request
         event.preventDefault();
     });
 
 });
+
+function postSuccess(res){
+    var resJSON = JSON.parse(res);
+    window.location.replace("./account-created.html?customerId=" + values.customerID + "&balance=" + values.initialCredit + "&accountId=" + resJSON.accountId + "&transactionId=" + resJSON.transactionId)
+}
+
+function postError(xhr){
+    console.log(xhr);
+    $('#error_message').text(xhr.responseText);
+    
+}
