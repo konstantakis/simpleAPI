4a9f0e60060549b87fcb1823c625757b8de0e5d1
Giannis Konstantakis <gkonstan93@gmail.com>
2019-12-06 16:03:26 +0200

    cross-origin request allow everything

--- server/src/main/java/gkonstan/api/server/App.java
+++ server/src/main/java/gkonstan/api/server/App.java
@@ -2,6 +2,10 @@ package gkonstan.api.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import gkonstan.api.server.database.utils.InitialData;
 import gkonstan.api.server.model.*;
@@ -14,5 +18,15 @@ public class App
         System.out.println( "Starting Server!" );
         InitialData.InsertFakeData();
         SpringApplication.run(App.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurerAdapter() {
+        @Override
+        public void addCorsMappings(CorsRegistry registry) {
+            registry.addMapping("/**").allowedOrigins("*");
+        }    
+    };
     }
 }
